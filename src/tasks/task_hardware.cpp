27d6671b61e6f0fb16854ae45edc425823c73930
Dawid Szewc <xephen20@gmail.com>
2024-04-02 14:44:18 +0200

    Interrupts handling before queue implementation

--- src/tasks/task_hardware.cpp
+++ src/tasks/task_hardware.cpp
@@ -1,15 +1,24 @@
 #include "tasks/task_hardware.hpp"
-#include <functional>
 
 const int BUTTONS[8] = {1, 2, 4, 5, 7, 8, 10, 11};
 const int ACTION_BUTTONS[4] = {3, 6, 9, 12};
 const int ENCODER[3] = {0, 18, 17}; // 1. Switch pin, 2. Pin Rotation A 3. Pin rotation B
 int debounce = 5;
 
-// TODO: Assign these values to global data storage, maybye create singleton class?
-int subMenuPosition = 1;
-int currentMenu = 1;
-int currentChannel = 1;
+struct oldValues {
+  int oldMenuPosition = 1;
+  int oldSubMenuPosition = 1;
+  int oldChannel = 1;
+  int oldEncoderPosition = 1;
+};
+struct oldValues oldValuesStruct;
+struct values {
+  int menuPosition;
+  int subMenuPosition;
+  int channel;
+  int encoderPosition;
+};
+struct values valuesStruct;
 
 EasyButton channelButtons[8] = {
     EasyButton(BUTTONS[0], debounce, true, false),
@@ -32,8 +41,9 @@ EasyButton menuButtons[4] = {
 RotaryEncoder encoder(ENCODER[2], ENCODER[1], RotaryEncoder::LatchMode::FOUR3);
 EasyButton encoderButton(ENCODER[0], debounce, true, false);
 
-void checkPosition() {
+void encoderHandler() {
   encoder.tick();
+  valuesStruct.encoderPosition = encoder.getPosition();
 }
 
 void eventsRead() {
@@ -57,36 +67,50 @@ void beginButtons() {
 }
 
 void handleCallbacks() {
-  menuButtons[0].onPressed([]() { (currentMenu = 1); });
-  menuButtons[1].onPressed([]() { (currentMenu = 2); });
-  menuButtons[2].onPressed([]() { (currentMenu = 3); });
-  menuButtons[3].onPressed([]() { (currentMenu = 4); });
-  channelButtons[0].onPressed([]() { (currentChannel = 1); });
-  channelButtons[1].onPressed([]() { (currentChannel = 2); });
-  channelButtons[2].onPressed([]() { (currentChannel = 3); });
-  channelButtons[3].onPressed([]() { (currentChannel = 4); });
-  channelButtons[4].onPressed([]() { (currentChannel = 5); });
-  channelButtons[5].onPressed([]() { (currentChannel = 6); });
-  channelButtons[6].onPressed([]() { (currentChannel = 7); });
-  channelButtons[7].onPressed([]() { (currentChannel = 8); });
-  encoderButton.onPressed([]() { (subMenuPosition += 1); });
+  menuButtons[0].onPressed([]() { (valuesStruct.menuPosition = 1); });
+  menuButtons[1].onPressed([]() { (valuesStruct.menuPosition = 2); });
+  menuButtons[2].onPressed([]() { (valuesStruct.menuPosition = 3); });
+  menuButtons[3].onPressed([]() { (valuesStruct.menuPosition = 4); });
+  channelButtons[0].onPressed([]() { (valuesStruct.channel = 1); });
+  channelButtons[1].onPressed([]() { (valuesStruct.channel = 2); });
+  channelButtons[2].onPressed([]() { (valuesStruct.channel = 3); });
+  channelButtons[3].onPressed([]() { (valuesStruct.channel = 4); });
+  channelButtons[4].onPressed([]() { (valuesStruct.channel = 5); });
+  channelButtons[5].onPressed([]() { (valuesStruct.channel = 6); });
+  channelButtons[6].onPressed([]() { (valuesStruct.channel = 7); });
+  channelButtons[7].onPressed([]() { (valuesStruct.channel = 8); });
+  encoderButton.onPressed([]() { (valuesStruct.subMenuPosition += 1); });
+}
+
+void updateDataStore () {
+  if (oldValuesStruct.oldMenuPosition != valuesStruct.menuPosition) {
+    printf("NewMenuPosition: %d\n", valuesStruct.menuPosition);
+    oldValuesStruct.oldMenuPosition = valuesStruct.menuPosition;
+  }
+  if (oldValuesStruct.oldChannel!= valuesStruct.channel) {
+    printf("New Channel Position: %d\n", valuesStruct.channel);
+    oldValuesStruct.oldChannel = valuesStruct.channel;
+  }
+  if (oldValuesStruct.oldSubMenuPosition!= valuesStruct.subMenuPosition) {
+    printf("New SubMenu Position: %d\n", valuesStruct.subMenuPosition);
+    oldValuesStruct.oldSubMenuPosition = valuesStruct.subMenuPosition;
+  }
+  if (oldValuesStruct.oldEncoderPosition!= valuesStruct.encoderPosition) {
+    printf("New SubMenu Position: %d\n", valuesStruct.encoderPosition);
+    oldValuesStruct.oldEncoderPosition= valuesStruct.encoderPosition;
+  }
 }
 
 void task_hardware(void *pvParameters) {
   pinMode(ENCODER[1], INPUT_PULLUP);
   pinMode(ENCODER[2], INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCODER[1]), checkPosition, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENCODER[2]), checkPosition, CHANGE);
-
+  attachInterrupt(digitalPinToInterrupt(ENCODER[1]), encoderHandler, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER[2]), encoderHandler, CHANGE);
   beginButtons();
   handleCallbacks();
-
   while (true) {
     eventsRead();
-    printf("Encoder value -> %ld \n", encoder.getPosition());
-    printf("Encoder click -> %ld \n", subMenuPosition);
-    printf("Menu value ->: %d\n", currentMenu);
-    printf("Channel value ->: %d\n", currentChannel);
+    updateDataStore();
     vTaskDelay(1);
   }
 }
