f264e6ebeae98a894198eaf1b5da9a54374282c1
Dawid Szewc <xephen20@gmail.com>
2024-04-02 14:03:48 +0200

    Fix flash.sh script

--- utilis/flash.sh
+++ utilis/flash.sh
@@ -1,3 +1,9 @@
-make
-cp src/pequencer.uf2 /Volumes/RPI-RP2
-killall NotificationCenter
+#!/bin/bash
+
+RPI_VOLUME=/Volumes/RPI-RP2
+
+ROOT_DIR=$(realpath "$(realpath $(dirname "$0"))/..")
+PROGRAM_NAME=`grep 'pico_set_program_name' $ROOT_DIR/CMakeLists.txt | grep -o '\".*\"' | tr -d '\"'`
+
+make -C $ROOT_DIR/build all
+find $ROOT_DIR/build -type f -name "${PROGRAM_NAME}.uf2" -exec install -m 644 {} $RPI_VOLUME \;
