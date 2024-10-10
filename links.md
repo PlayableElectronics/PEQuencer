709a0133de48a859f3380c9a9b7ae462c2909386
Daniel Górny <reverbrick@interia.pl>
2024-10-10 21:49:47 +0200

    working asyncio skeleton

---
+++ links.md
@@ -0,0 +1,2 @@
+https://github.com/adafruit/Adafruit_CircuitPython_MIDI/tree/main/adafruit_midi
+
--- macropad.py
+++ macropad.py
@@ -8,22 +8,12 @@ from time import ticks_ms
 from micropython import const
 from machine import Pin
 
-
-PIN_OLED_CS = 22
-PIN_OLED_DC = 24
-PIN_OLED_RST = 23
-
-PIN_LED = 13
-PIN_NEOPIXEL = 19
-
 PIN_ROTA = 17
 PIN_ROTB = 18
 
 PIN_SPEAKER = 16
 PIN_SPEAKER_SD = 14
 
-
-# https://gist.github.com/jedie/8564e62b0b8349ff9051d7c5a1312ed7
 class Button:
     """
     Debounced pin handler
@@ -53,19 +43,3 @@ class Button:
 
     def value(self):
         return self.pin.value()
-
-
-def make_display():
-    from machine import SPI, Pin
-    from sh1106 import SH1106_SPI
-
-    spi = SPI(1, sck=Pin(26, Pin.OUT), mosi=Pin(27, Pin.OUT),
-              polarity=SH1106_SPI.POL, phase=SH1106_SPI.PHA)
-
-    display = SH1106_SPI(128, 64, spi, Pin(PIN_OLED_DC, Pin.OUT),
-                         cs=Pin(PIN_OLED_CS, Pin.OUT),
-                         res=Pin(PIN_OLED_RST, Pin.OUT))
-    display.fill(0)
-    display.rotate(True, True)
-
-    return display
